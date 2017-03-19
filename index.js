f829679a16775debe007b25e17af251c3a86eb28
MrLoh <flojoto@hotmail.com>
2017-03-19 19:38:53 +0100

    minimal updates

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-fs = require('fs')
-readCSV = require('csv-load-sync')
-
-let data = readCSV('../inge-geburtstagsspenden.csv');
-data = data.map((d) => Object.assign(d, {address: d.address.replace('\\n', '\n')}))
-
 const generate_receipt = require('./donation-receipt-generator').generate
 
-for (let info of data) {
-    generate_receipt(info, `Spendenbescheinigung_${info.filename}_2017.pdf`)
-}
+// fs = require('fs')
+// readCSV = require('csv-load-sync')
+
+// let data = readCSV('../inge-geburtstagsspenden.csv');
+// data = data.map((d) => Object.assign(d, {address: d.address.replace('\\n', '\n')}))
+
+// for (let info of data) {
+//     generate_receipt(info, `Spendenbescheinigung_${info.filename}_2017.pdf`)
+// }
 
 generate_receipt({
     name: 'Tobias Lohse',
@@ -16,3 +16,10 @@ generate_receipt({
     amount: 145.50,
     date: '1.1.2020'
 })
+
+generate_receipt({
+    name: 'gut.org gemeinnützige Aktiengesellschaft',
+    address: 'Schlesische Straße 26\n10997 Berlin',
+    amount: 285.00,
+    date: '28.6.2016'
+}, 'betterplace_2016.pdf')
